5a814dac0680ac6270909a2fac8c64bb37458785
sh-ikeda <sikeda@dbcls.rois.ac.jp>
2024-07-03 14:56:44 +0900

    add script to rename rdf-config output

---
+++ script/camel2snake.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -euo pipefail
+
+for f in *graphql; do
+    if [ $f = "index.graphql" ]; then
+       continue
+    else
+        before=${f%.graphql}
+        after=`echo $before | sed -E 's/(.)([A-Z])/\1_\2/g' | awk '{print tolower($0)}'`
+        sed -i -e "s/$before/$after/g" $f
+        sed -i -e "s/^  $before\(/  $after\(/g" index.graphql
+        sed -i -e "s/\[$before\]/\[$after\]/g" index.graphql
+        mv $f $after.graphql
+    fi
+done
