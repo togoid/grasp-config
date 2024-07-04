ac4c032fef924764aa149e5e296e7ce9fd46ac94
sh-ikeda <sikeda@dbcls.rois.ac.jp>
2024-07-04 15:24:30 +0900

    bugfix

--- script/camel2snake.sh
+++ script/camel2snake.sh
@@ -9,8 +9,8 @@ for f in *graphql; do
         before=${f%.graphql}
         after=`echo $before | sed -E 's/(.)([A-Z])/\1_\2/g' | awk '{print tolower($0)}'`
         sed -i -e "s/$before/$after/g" $f
-        sed -i -e "s/^  $before\(/  $after\(/g" index.graphql
-        sed -i -e "s/\[$before\]/\[$after\]/g" index.graphql
+        sed -i -E "s/^  $before\(/  $after(/g" index.graphql
+        sed -i -E "s/\[$before\]/\[$after\]/g" index.graphql
         mv $f $after.graphql
     fi
 done
